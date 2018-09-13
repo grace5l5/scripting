f714d3ff3f6443df5075f140358a31fa59c7f35a
Grace5l5 <gs.leungshing@gmail.com>
2018-09-13 16:31:07 -0700

    python.py fixed files new pages.html using python sent to docs1 folder

--- build.py
+++ build.py
@@ -1,5 +1,23 @@
-filenames = ['file1.txt', 'file2.txt', 'file3.txt']
-with open('output_file', 'w') as outfile:
+filenames = ['templates/top.html', 'content/index.html', 'templates/bottom.html']
+with open('docs1/index.html', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            outfile.write(infile.read())
+
+filenames = ['templates/top.html', 'content/contact.html', 'templates/bottom.html']
+with open('docs1/contact.html', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            outfile.write(infile.read())
+
+filenames = ['templates/top.html', 'content/blogs.html', 'templates/bottom.html']
+with open('docs1/blogs.html', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            outfile.write(infile.read())
+
+filenames = ['templates/top.html', 'content/projects.html', 'templates/bottom.html']
+with open('docs1/projects.html', 'w') as outfile:
     for fname in filenames:
         with open(fname) as infile:
             outfile.write(infile.read())
